ffe58a257b6f0bdd84f4fbf631a433997faddb67
Duy Trong <quangtrung20002012@gmail.com>
2022-01-20 17:02:19 +0700

    after some "fun"

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,16 @@ import AddTodoRedux from "./Components/ReduxStore/AddTodoRedux";
 import HomeRedux from "./Components/ReduxStore/HomeRedux";
 import { store } from "./Components/ReduxStore/store/store";
 import TodoListRedux from "./Components/ReduxStore/TodoListRedux";
+import AddTodoRTK from "./Components/RTK Query/AddTodoRTK";
+import HomeRTK from "./Components/RTK Query/HomeRTK";
+import { storeRTK } from "./Components/RTK Query/store/storeRTK";
+import TodoListRTK from "./Components/RTK Query/TodoListRTK";
 // import TodoReduxApp from "./Components/ReduxStore/TodoReduxApp";
 
 function App() {
   return (
     <>
-      <Routes>
+      {/* <Routes>
         <Route path="basic" element={<TodoApp />} />
         <Route path="context" element={<TodoContextApp />} />
       </Routes>
@@ -22,7 +26,14 @@ function App() {
           <Route path="redux/addTodo" element={<AddTodoRedux />} />
           <Route path="redux/viewTodoList" element={<TodoListRedux />} />
         </Routes>
-      </Provider>
+      </Provider> */}
+      {/* <Provider store={storeRTK}>
+        <Routes>
+          <Route path="RTK" element={<HomeRTK />} />
+          <Route path="RTK/addTodoRTK" element={<AddTodoRTK />} />
+          <Route path="RTK/viewTodoListRTK" element={<TodoListRTK />} />
+        </Routes>
+      </Provider> */}
     </>
   );
 }
